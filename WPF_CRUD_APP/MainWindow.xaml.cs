c0a1785f6309ee79081bdb49626f7d7002384361
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2020-03-02 20:41:11 -0500

    added click event for save button and saving book info entered to db

--- WPF_CRUD_APP/MainWindow.xaml.cs
+++ WPF_CRUD_APP/MainWindow.xaml.cs
@@ -20,9 +20,44 @@ namespace WPF_CRUD_APP
     /// </summary>
     public partial class MainWindow : Window
     {
+        //creating a DB object
+        public BookDBEntities db = new BookDBEntities();
+        public static DataGrid data;
         public MainWindow()
         {
             InitializeComponent();
+            datagrid();
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            //here the new Book refers to the class
+            var book = new Book
+            {
+                Title=txtTitle.Text,
+                Author=txtAuthor.Text,
+                Publication_Date = (DateTime)txtDate.SelectedDate,
+                ISBN =txtISBN.Text
+            };
+            //here the Books refers to the DbSet
+            db.Books.Add(book);
+            db.SaveChanges();
+            datagrid();
+            clearFields();
+        }
+
+        public void datagrid()
+        {
+            MyBooksGrid.ItemsSource = db.Books.ToList();
+            data = MyBooksGrid;
+        }
+
+        public void clearFields()
+        {
+            txtTitle.Clear();
+            txtAuthor.Clear();
+            txtISBN.Clear();
+            txtDate.Text = "";
         }
     }
 }
